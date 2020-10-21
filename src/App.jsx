d3675d8c9aa881e65694fd504825d9072e733378
Shamed Calderon <arzu.calderon@gmail.com>
2020-10-21 00:44:49 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App(params) {
+    return (
+        <div className="app">
+            <h1>Hola React</h1>
+        </div>
+    );
+}
+
+export default App;
